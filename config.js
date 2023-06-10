ceda1bc3b20f15779e8e3b421b476afae9322e51
olega211 <106370448+olega211@users.noreply.github.com>
2023-06-11 00:23:24 +0700

    f

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-// export const API_DOMAIN = "http://hetts.meatqwtest.ga";
-export const API_DOMAIN = "http://localhost:8000";
+export const API_DOMAIN = "http://hetts.meatqwtest.ga";
+// export const API_DOMAIN = "http://localhost:8000";
